c8af701658ec2f31076c3a06d5b95326503fc84e
lucas cerqueira do nascimento <terrarialucas123@gmail.com>
2025-10-05 13:32:36 -0300

    .

--- src/components/Conteudo.jsx
+++ src/components/Conteudo.jsx
@@ -1,8 +1,32 @@
+import React, { useState, useEffect } from "react";
+
 function Conteudo() {
+  // Importa todas as imagens tudo dentro de um objeto
+  const imgFiles = import.meta.glob("../assets/drawings/*.jpg", {
+    eager: true,
+  });
+
+  // Transforma o objeto em uma array de URLs
+  const UrlsList = Object.values(imgFiles).map((mod) => mod.default || mod);
+
+  const [ImageAtual, setImageAtual] = useState(null);
+
+  useEffect(() => {
+    if (UrlsList.length === 0) return; // Checa se tem Imagens
+    //intevalo par mudar de imagens acada 5s
+    setTimeout(() => {
+      const RandomKey = Math.floor(Math.random() * UrlsList.length);
+      //Faz aleatoriedade dentro da array
+      const UrlSelect = UrlsList[RandomKey];
+
+      setImageAtual(UrlSelect); // Altera o state
+    }, 5000);
+  }, []);
+
   return (
     <>
       <div className="imgs">
-        <img src="styles/img/gif do tails .gif" alt="" />
+        <img src={ImageAtual} alt="Imagens" className="w-[250px] h-[250px]" />
       </div>
 
       <div className="link">
